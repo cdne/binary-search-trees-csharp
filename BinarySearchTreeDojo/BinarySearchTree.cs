8cb641e0c49c1e0d8a6663b0901833e1da8c3947
Claudiu <cduneata@gmail.com>
2020-08-24 20:51:45 +0300

    Add node class

--- BinarySearchTreeDojo/BinarySearchTree.cs
+++ BinarySearchTreeDojo/BinarySearchTree.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.AccessControl;
 
 namespace BinarySearchTreeDojo
 {
     public class BinarySearchTree
     {
+        private readonly Node root;
+
         private BinarySearchTree()
         {
             // private constructor so it can't be instantiated like this
+            root = null;
         }
 
         public static BinarySearchTree Build(List<int> elements)
@@ -28,5 +32,17 @@ namespace BinarySearchTreeDojo
         public void Remove(int toRemove) {
             // TODO removes an element. Throws an error if its not on the tree.
         }     
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node left, right;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+        
     }
 }
